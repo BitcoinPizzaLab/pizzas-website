989e29ea4bb4e21d2553df4b3351f07680bf22c0
unknown <bitcoinnftpizzalab@gmail.com>
2023-05-15 02:19:40 +0800

    refactor(modal): replace useLatest and useUpdate hooks with useRef and useMount hooks feat(modal): add drag functionality to modal window title bar

--- app/libs/components/modal/index.tsx
+++ app/libs/components/modal/index.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './modal.css'
 import { Box } from '@chakra-ui/react'
 import { motion } from 'framer-motion'
-import { useLatest, useUpdate } from 'react-use'
+import { useMount } from 'react-use'
 
 interface ModalProps {
   isOpen: boolean
@@ -19,32 +19,26 @@ export const Modal = (props: ModalProps) => {
     },
   } = props
   const [dragAble, setDragAble] = useState(false)
-  const latestCount = useLatest(dragAble)
-  const update = useUpdate()
+  const dragElement = useRef<HTMLDivElement | null>(null)
+
+  useMount(() => {
+    dragElement.current?.addEventListener('pointerdown', () => {
+      setDragAble(true)
+    })
+
+    dragElement.current?.addEventListener('pointerup', () => {
+      setDragAble(false)
+    })
+  })
 
   if (!isOpen) return null
 
   return (
-    <motion.div
-      dragPropagation={false}
-      dragTransition={{ bounceStiffness: 600, bounceDamping: 10 }}
-      drag={latestCount.current}
-      dragMomentum={false}
-      className='window'
-      animate={{ opacity: isOpen ? 1 : 0 }}
-    >
-      <Box
-        onMouseOver={() => {
-          setDragAble(true)
-          update()
-        }}
-        onMouseUpCapture={() => {
-          setDragAble(false)
-          update()
-        }}
-        className='title-bar'
-      >
-        <Box className='title-bar-text'>A Window With Stuff In It</Box>
+    <motion.div dragPropagation={false} dragTransition={{ bounceStiffness: 600, bounceDamping: 10 }} drag={dragAble} dragMomentum={false} className='window' animate={{ opacity: isOpen ? 1 : 0 }}>
+      <Box ref={dragElement} className='title-bar'>
+        <Box userSelect='none' className='title-bar-text'>
+          A Window With Stuff In It
+        </Box>
         <Box className='title-bar-controls'>
           <button aria-label='Minimize'></button>
           <button aria-label='Maximize'></button>
